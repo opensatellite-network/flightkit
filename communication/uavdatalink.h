e907df02567347a4baf42ed856fc1f4194d6aaf6
krishpranav <krisna.pranav@gmail.com>
2023-09-05 19:02:56 +0530

    communcation: uavdatalink

--- communication/uavdatalink.h
+++ communication/uavdatalink.h
@@ -12,11 +12,106 @@
 #ifndef _COMMUNICATION_UAV_DATA_LINK_H
 #define _COMMUNICATION_UAV_DATA_LINK_H
 
+#include <stdint.h>
+
+/**
+ * @param IDA 
+ * @param IDB 
+ * @param PAYLOADLENGTH 
+ * @param PAYLOAD 
+ * @param byteStreamOut 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_pack(const uint8_t IDA, const uint8_t IDB, const uint8_t PAYLOADLENGTH, const uint8_t *PAYLOAD, uint8_t *byteStreamOut);
+
+/**
+ * @param receivedPacket 
+ * @param receivedPacketLength 
+ * @param packetHeader 
+ * @param payload 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_unpack(uint8_t *receivedPacket, uint8_t receivedPacketLength, uint8_t *packetHeader, uint8_t *payload);
+
+/**
+ * @param dataIn 
+ * @param dataInLength 
+ * @param dataOut 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_encodeCOBS(const uint8_t *dataIn, const uint8_t dataInLength, uint8_t *dataOut);
+
+/**
+ * @param dataIn 
+ * @param dataInLength 
+ * @param dataOut 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_decodeCOBS(const uint8_t *dataIn, const uint8_t dataInLength, uint8_t *dataOut);
+
+/**
+ * @param payload 
+ * @param payloadLength 
+ * @param extractedFloats 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_payloadToFloats(const uint8_t *payload, const uint8_t payloadLength, float *extractedFloats);
+
+/**
+ * @param arr 
+ * @param arrLength 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_checksum(const uint8_t *arr, const uint8_t arrLength);
+
 typedef union FloatConverterUnion 
 {
     float val;
     uint8_t bytes[4];
 } FloatConverter;
+
+/**
+ * @param IDA 
+ * @param IDB 
+ * @param PAYLOADLENGTH 
+ * @param PAYLOAD 
+ * @param byteStreamOut 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_pack(const uint8_t IDA, const uint8_t IDB, const uint8_t PAYLOADLENGTH, const uint8_t *PAYLOAD, uint8_t *byteStreamOut);
+
+/**
+ * @param receivedPacket 
+ * @param receivedPacketLength 
+ * @param packetHeader 
+ * @param payload 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_unpack(uint8_t *receivedPacket, uint8_t receivedPacketLength, uint8_t *packetHeader, uint8_t *payload);
+
+/**
+ * @param payload 
+ * @param payloadLength 
+ * @param extractedFloats 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_payloadToFloats(const uint8_t *payload, const uint8_t payloadLength, float *extractedFloats);
+
+/**
+ * @param dataIn 
+ * @param dataInLength 
+ * @param dataOut 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_encodeCOBS(const uint8_t *dataIn, const uint8_t dataInLength, uint8_t *dataOut);
+
+/**
+ * @param dataIn 
+ * @param dataInLength 
+ * @param dataOut 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_decodeCOBS(const uint8_t *dataIn, const uint8_t dataInLength, uint8_t *dataOut);
 
 /**
  * @param arr 
