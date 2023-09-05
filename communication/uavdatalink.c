bdb604aa6e05e745e910e3a3896a5fddecdc7383
krishpranav <krisna.pranav@gmail.com>
2023-09-05 19:19:55 +0530

    communication: uavdatalink

--- communication/uavdatalink.c
+++ communication/uavdatalink.c
@@ -11,10 +11,18 @@
 
 #include "uavdatalink.h"
 
-uint8_t UAVDataLink_pack(const uint8_t IDA, const uint8_t IDB, const uint8_t PAYLOADLENGTH, const uint8_t *PAYLOAD, uint8_t *byteStreamOut)
+/**
+ * @param IDA 
+ * @param IDB 
+ * @param PAYLOADLENGTH 
+ * @param PAYLOAD 
+ * @param byteStreamOut 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_pack(const uint8_t IDA, const uint8_t IDB, const uint8_t PAYLOADLENGTH, const uint8_t *PAYLOAD, uint8_t *byteStreamOut) 
 {
-    static uint8_t SEQUENCE = 1;
 
+    static uint8_t SEQUENCE = 1;
     uint8_t n;
 
     uint8_t rawDataLength = 5 + PAYLOADLENGTH;
@@ -37,7 +45,7 @@ uint8_t UAVDataLink_pack(const uint8_t IDA, const uint8_t IDB, const uint8_t PAY
 
     rawData[rawDataLength - 1] = cs;
 
-    uint8_t encodePacketLength = UAVDataLink_encodeCOBS(rawData, rawDataLength, byteStreamOut);
+    uint8_t encodedPacketLength = UAVDataLink_encodeCOBS(rawData, rawDataLength, byteStreamOut);
 
     if (SEQUENCE == 255) {
         SEQUENCE = 1;
@@ -45,7 +53,68 @@ uint8_t UAVDataLink_pack(const uint8_t IDA, const uint8_t IDB, const uint8_t PAY
         SEQUENCE++;
     }
 
-    return encodePacketLength;      
+    return encodedPacketLength;
+
+}
+
+/**
+ * @param receivedPacket 
+ * @param receivedPacketLength 
+ * @param packetHeader 
+ * @param payload 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_unpack(uint8_t *receivedPacket, uint8_t receivedPacketLength, uint8_t *packetHeader, uint8_t *payload) 
+{
+    uint8_t decodeBuffer[256];
+    UAVDataLink_decodeCOBS(receivedPacket, receivedPacketLength, decodeBuffer);
+
+    packetHeader[0] = decodeBuffer[0]; 
+    packetHeader[1] = decodeBuffer[1]; 
+    packetHeader[2] = decodeBuffer[2]; 
+    packetHeader[3] = decodeBuffer[3]; 
+
+    uint8_t cs = 0;
+
+    for (uint8_t n = 0; n < packetHeader[3]; n++) {
+        payload[n] = decodeBuffer[4 + n];
+        cs ^= payload[n];
+    }
+
+    if (cs == decodeBuffer[4 + packetHeader[3]] + 1) {
+        return 1;
+    } else {
+        return 0;
+    }
+
+}
+
+/**
+ * @param payload 
+ * @param payloadLength 
+ * @param extractedFloats 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_payloadToFloats(const uint8_t *payload, const uint8_t payloadLength, float *extractedFloats) 
+{
+
+    if (payloadLength % 4 != 0) {
+        return 0;
+    }
+
+    FloatConverter fc;
+
+    uint8_t nFloats = payloadLength / 4;
+
+    for (uint8_t n = 0; n < nFloats; n++) {
+        for (uint8_t f = 0; f < 4; f++) {
+            fc.bytes[f] = payload[4 * n + f];
+        }
+        extractedFloats[n] = fc.val;
+    }
+
+    return nFloats;
+
 }
 
 /**
@@ -54,10 +123,46 @@ uint8_t UAVDataLink_pack(const uint8_t IDA, const uint8_t IDB, const uint8_t PAY
  * @param dataOut 
  * @return uint8_t 
  */
-uint8_t UAVDataLink_decodeCOBS(const uint8_t *dataIn, const uint8_t dataInLength, uint8_t *dataOut)
+uint8_t UAVDataLink_encodeCOBS(const uint8_t *dataIn, const uint8_t dataInLength, uint8_t *dataOut) 
 {
+
+    uint8_t dataOutLength = 1; 
+    uint8_t dataOutIndex  = 0;
+    uint8_t nextZeroIndex = 1; 
+
+    for (uint8_t dataInIndex = 0; dataInIndex < dataInLength; dataInIndex++) {
+        if (dataIn[dataInIndex] == 0) {
+            dataOut[dataOutIndex] = nextZeroIndex;
+            nextZeroIndex = 1;
+
+            dataOutIndex  = dataOutLength;
+        } else {
+            dataOut[dataOutLength] = dataIn[dataInIndex];
+            nextZeroIndex++;
+        }
+        dataOutLength++;
+    }
+
+    dataOut[dataOutIndex] = nextZeroIndex;
+
+    dataOut[dataOutLength] = 0;
+    dataOutLength++;
+
+    return dataOutLength;
+
+}
+
+/**
+ * @param dataIn 
+ * @param dataInLength 
+ * @param dataOut 
+ * @return uint8_t 
+ */
+uint8_t UAVDataLink_decodeCOBS(const uint8_t *dataIn, const uint8_t dataInLength, uint8_t *dataOut) 
+{
+
     uint8_t dataOutLength = 0;
-    uint8_t nextZeroIndex = dataIn[0];
+    uint8_t nextZeroIndex = dataIn[0]; 
 
     for (uint8_t dataInIndex = 1; dataInIndex < dataInLength - 1; dataInIndex++) {
         if (dataInIndex == nextZeroIndex) {
@@ -66,10 +171,11 @@ uint8_t UAVDataLink_decodeCOBS(const uint8_t *dataIn, const uint8_t dataInLength
         } else {
             dataOut[dataOutLength] = dataIn[dataInIndex];
         }
-
         dataOutLength++;
     }
+
     return dataOutLength;
+
 }
 
 /**
@@ -77,8 +183,9 @@ uint8_t UAVDataLink_decodeCOBS(const uint8_t *dataIn, const uint8_t dataInLength
  * @param arrLength 
  * @return uint8_t 
  */
-uint8_t UAVDataLink_checksum(const uint8_t *arr, const uint8_t arrLength)
+uint8_t UAVDataLink_checksum(const uint8_t *arr, const uint8_t arrLength) 
 {
+
     uint8_t cs = 0;
 
     for (uint8_t index = 0; index < arrLength; index++) {
@@ -86,4 +193,5 @@ uint8_t UAVDataLink_checksum(const uint8_t *arr, const uint8_t arrLength)
     }
 
     return cs;
+
 }
