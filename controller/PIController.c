aa8eff71a98ad01bc03ac65151d25280214fb55a
krishpranav <krisna.pranav@gmail.com>
2023-09-05 21:53:03 +0530

    controller: PIController

--- controller/PIController.c
+++ controller/PIController.c
@@ -10,7 +10,6 @@
  */
 
 #include "PIController.h"
-#include "controller/PIController.h"
 
 /**
  * @param ctrl 
@@ -19,27 +18,27 @@
  * @param limMin 
  * @param limMax 
  */
-void PI_Init(PIController *ctrl, float Kp, float Ki, float limMin, float limMax)
+void PI_Init(PIController *ctrl, float Kp, float Ki, float limMin, float limMax) 
 {
-    ctrl->Kp = Kp;
-    ctrl->Ki = Ki;
-    ctrl->limMin = limMin;
-    ctrl->limMax = limMax;
-    ctrl->integrator = 0.0f;
-    ctrl->prevError = 0.0f;
-    ctrl->output = 0.0f;
-    ctrl->limMinFF = 0.0f;
-    ctrl->limMaxFF = 0.0f;
+	ctrl->Kp = Kp;
+	ctrl->Ki = Ki;	
+	ctrl->limMin = limMin;
+	ctrl->limMax = limMax;
+	ctrl->integrator = 0.0f;
+	ctrl->prevError  = 0.0f;
+	ctrl->output     = 0.0f;
+	ctrl->limMinFF = 0.0f;
+	ctrl->limMaxFF = 0.0f;
 }
 
 /**
  * @param ctrl 
  */
-void PI_Reset(PIController *ctrl)
+void PI_Reset(PIController *ctrl) 
 {
-    ctrl->integrator = 0.0f;
-    ctrl->prevError = 0.0f;
-    ctrl->output = 0.0f;
+	ctrl->integrator = 0.0f;
+	ctrl->prevError  = 0.0f;
+	ctrl->output     = 0.0f;
 }
 
 /**
@@ -48,11 +47,11 @@ void PI_Reset(PIController *ctrl)
  * @param limMinFF 
  * @param limMaxFF 
  */
-void PI_SetFF(PIController *ctrl, float Kff, float limMinFF, float limMaxFF)
+void PI_SetFF(PIController *ctrl, float Kff, float limMinFF, float limMaxFF) 
 {
-    ctrl->Kff = Kff;
-    ctrl->limMinFF = limMaxFF;
-    ctrl->limMaxFF = limMaxFF;
+	ctrl->Kff = Kff;
+	ctrl->limMinFF = limMinFF;
+	ctrl->limMaxFF = limMaxFF;
 }
 
 /**
@@ -62,20 +61,49 @@ void PI_SetFF(PIController *ctrl, float Kff, float limMinFF, float limMaxFF)
  * @param T 
  * @return float 
  */
-float PI_Update(PIController *ctrl, float setpoint, float measurement, float T)
+float PI_Update(PIController *ctrl, float setpoint, float measurement, float T) 
 {
-    float error = setpoint - measurement;
-    float proportional = ctrl->Kp * error;
+	float error = setpoint - measurement;
+	float proportional = ctrl->Kp * error;
+	float ff = ctrl->Kff * setpoint;
+	
+	if (ff > ctrl->limMaxFF) {
+		ff = ctrl->limMaxFF;
+	} else if (ff < ctrl->limMinFF) {
+		ff = ctrl->limMinFF;
+	}
 
-    float ff = ctrl->Kff * setpoint;
+	ctrl->integrator = ctrl->integrator + 0.5f * T * ctrl->Ki * (error + ctrl->prevError);
 
-    if (ff > ctrl->limMaxFF) {
-        ff = ctrl->limMaxFF;
-    } else if (ff < ctrl->limMinFF) {
-        ff = ctrl->limMinFF;
-    }
-
-    ctrl->integrator = ctrl->integrator + 0.5f * T * ctrl->Ki * (error + ctrl->prevError);
-
-    return ctrl->output;
+	float limMinInt, limMaxInt;
+	
+	if (ctrl->limMax - proportional - ff > 0.0f) {
+		limMaxInt = ctrl->limMax - proportional;
+	} else {
+		limMaxInt = 0.0f;
+	}
+	
+	if (ctrl->limMin - proportional - ff < 0.0f) {
+		limMinInt = ctrl->limMin - proportional;
+	} else {
+		limMinInt = 0.0f;
+	}
+	
+	if (ctrl->integrator > limMaxInt) {
+		ctrl->integrator = limMaxInt;
+	} else if (ctrl->integrator < limMinInt) {
+		ctrl->integrator = limMinInt;
+	}
+	
+	ctrl->output = proportional + ff + ctrl->integrator;
+	
+	if (ctrl->output > ctrl->limMax) {
+		ctrl->output = ctrl->limMax;
+	} else if (ctrl->output < ctrl->limMin) {
+		ctrl->output = ctrl->limMin;
+	}
+	
+	ctrl->prevError = error;
+	
+	return ctrl->output;
 }
