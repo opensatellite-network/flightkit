d16284d77609a58bfca5030e2a3b64fd4b33093f
krishpranav <krisna.pranav@gmail.com>
2023-09-06 11:20:45 +0530

    filter: cfilter[FIR]

---
+++ filter/cfilter/FIR.h
@@ -0,0 +1,62 @@
+/**
+ * @file FIR.h
+ * @author Krisna Pranav
+ * @brief fir
+ * @version 1.0
+ * @date 2023-09-06
+ * 
+ * @copyright Copyright (c) 2023 opensatellite-network develoeprs, Krisna Pranav
+ * 
+ */
+
+#ifndef FILTER_FIR_FILTER_H
+#define FILTER_FIR_FILTER_H
+
+#include <stdint.h>
+
+typedef struct
+{
+    float out;
+    float *coeff;
+    float *buf;
+    uint8_t order;
+    uint8_t putIndex;
+} FIRFilter;
+
+/**
+ * @param filt 
+ * @param coeff 
+ * @param buf 
+ * @param order 
+ */
+void FIRFilter_Init(FIRFilter *filt, float *coeff, float *buf, const uint8_t order)
+{
+    filt->out = 0.0f;
+    filt->coeff = coeff;
+    filt->order = order;
+    filt->putIndex = 0;
+
+    for (uint8_t n = 0; n < order; n++) {
+        filt->buf[n] = 0.0f;
+    }
+}
+
+/**
+ * @param filt 
+ * @param in 
+ * @return float 
+ */
+float FIRFilter_Update(FIRFilter *filt, float in)
+{
+    filt->buf[filt->putIndex] = in;
+
+    uint8_t getIndex = filt->putIndex;
+
+    filt->out = 0.0f;
+
+    filt->putIndex++;
+
+    return filt->out;
+}
+
+#endif 
